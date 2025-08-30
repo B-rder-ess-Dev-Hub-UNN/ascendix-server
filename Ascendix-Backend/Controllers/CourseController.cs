d991356102a651f94fbfefafee925de6fb5bdec9
engr-Godswillochi <ochigodswill@gmail.com>
2025-08-30 05:35:09 +0100

    comleted the crud functionalities for courses

--- Ascendix-Backend/Controllers/CourseController.cs
+++ Ascendix-Backend/Controllers/CourseController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ascendix_Backend.Dto.CourseDto;
 using Ascendix_Backend.Interfaces;
+using Ascendix_Backend.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ascendix_Backend.Controllers
@@ -21,6 +23,47 @@ namespace Ascendix_Backend.Controllers
         {
             var course = await _courseRepo.GetAll();
             return Ok(course);
+        }
+
+        [HttpGet("get/{id:guid}")]
+        public async Task<IActionResult> getById([FromRoute] Guid id)
+        {
+            var course = await _courseRepo.GetById(id);
+            if (course == null) return StatusCode(404, "Course Not Found");
+            return Ok(course.fromCourse());
+        }
+
+        [HttpGet("getbylibraryId/{id:guid}")]
+        public async Task<IActionResult> getByLibraryId([FromRoute] Guid id)
+        {
+            var course = await _courseRepo.GetCourseByLibrary(id);
+            return Ok(course);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> create([FromBody] CreateCourse create)
+        {
+            var course = create.toCourse();
+            await _courseRepo.Create(course);
+            return Ok(course.fromCourse());
+        }
+
+        [HttpPatch("update/{id:guid}")]
+        public async Task<IActionResult> update([FromRoute] Guid id, [FromBody] UpdateCourse update)
+        {
+            var course = await _courseRepo.Update(id, update);
+            if (course == null) return StatusCode(404, "Course not Found");
+
+            return Ok(course.fromCourse());
+        }
+
+        [HttpDelete("delete/{id:guid}")]
+        public async Task<IActionResult> delete([FromRoute] Guid id)
+        {
+            var course = await _courseRepo.DeleteById(id);
+            if (course == null) return StatusCode(404, "Course Not Found");
+
+            return StatusCode(200, "Course Successfully Deleted");
         }
     }
 }
